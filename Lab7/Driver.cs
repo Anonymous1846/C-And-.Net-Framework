c974755481ac6df61fe7b9fa5f9e7881f38ce55b
Sharath Sunil <alphatrinity17@gmail.com>
2020-10-06 08:53:32 +0530

    Lab 7 Initialized with Driver Class, Number of Questions not known as of now !

---
+++ Lab7/Driver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    class Driver
+    {
+        /*The Driver Class to call the other classes.
+         Individual Classes contains individual questions !*/
+        static void Main(string[] args)
+        {
+            int choice;
+            do {
+                Console.WriteLine("1)Question1\n2)Question2)\nExit");
+                choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    default:
+                        break:
+                }
+            } while (choice!=5);
+        }
+    }
+}
