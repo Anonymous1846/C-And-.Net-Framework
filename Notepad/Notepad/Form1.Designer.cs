5c9999824c54e4f4773e6ac2776ab62575be3423
Sharath Sunil <alphatrinity17@gmail.com>
2020-10-29 15:40:22 +0530

    Notepad is working as expected, the work is to be done on the save and open dialog boxes no errors as of now !

---
+++ Notepad/Notepad/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Notepad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.file = new System.Windows.Forms.ToolStripMenuItem();
+            this.edit = new System.Windows.Forms.ToolStripMenuItem();
+            this.format = new System.Windows.Forms.ToolStripMenuItem();
+            this.about = new System.Windows.Forms.ToolStripMenuItem();
+            this.open = new System.Windows.Forms.ToolStripMenuItem();
+            this.save = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.find = new System.Windows.Forms.ToolStripMenuItem();
+            this.replaceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pasteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.googleSearchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.privacyPolicy = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoTool = new System.Windows.Forms.ToolStripMenuItem();
+            this.textArea = new System.Windows.Forms.RichTextBox();
+            this.fontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.file,
+            this.edit,
+            this.format,
+            this.about});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(653, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // file
+            // 
+            this.file.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.open,
+            this.save,
+            this.saveAs,
+            this.exit});
+            this.file.Name = "file";
+            this.file.Size = new System.Drawing.Size(46, 24);
+            this.file.Text = "File";
+            // 
+            // edit
+            // 
+            this.edit.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.find,
+            this.replaceToolStripMenuItem,
+            this.copyToolStripMenuItem,
+            this.pasteToolStripMenuItem});
+            this.edit.Name = "edit";
+            this.edit.Size = new System.Drawing.Size(49, 24);
+            this.edit.Text = "Edit";
+            // 
+            // format
+            // 
+            this.format.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.googleSearchToolStripMenuItem,
+            this.fontToolStripMenuItem});
+            this.format.Name = "format";
+            this.format.Size = new System.Drawing.Size(70, 24);
+            this.format.Text = "Format";
+            // 
+            // about
+            // 
+            this.about.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.privacyPolicy,
+            this.infoTool});
+            this.about.Name = "about";
+            this.about.Size = new System.Drawing.Size(64, 24);
+            this.about.Text = "About";
+            // 
+            // open
+            // 
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(224, 26);
+            this.open.Text = "Open";
+            // 
+            // save
+            // 
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(224, 26);
+            this.save.Text = "Save";
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // saveAs
+            // 
+            this.saveAs.Name = "saveAs";
+            this.saveAs.Size = new System.Drawing.Size(224, 26);
+            this.saveAs.Text = "Save As";
+            // 
+            // exit
+            // 
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(224, 26);
+            this.exit.Text = "Exit";
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // find
+            // 
+            this.find.Name = "find";
+            this.find.Size = new System.Drawing.Size(224, 26);
+            this.find.Text = "Find";
+            this.find.Click += new System.EventHandler(this.findToolStripMenuItem_Click);
+            // 
+            // replaceToolStripMenuItem
+            // 
+            this.replaceToolStripMenuItem.Name = "replaceToolStripMenuItem";
+            this.replaceToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.replaceToolStripMenuItem.Text = "Replace";
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.copyToolStripMenuItem.Text = "Copy";
+            // 
+            // pasteToolStripMenuItem
+            // 
+            this.pasteToolStripMenuItem.Name = "pasteToolStripMenuItem";
+            this.pasteToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.pasteToolStripMenuItem.Text = "Paste";
+            // 
+            // googleSearchToolStripMenuItem
+            // 
+            this.googleSearchToolStripMenuItem.Name = "googleSearchToolStripMenuItem";
+            this.googleSearchToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.googleSearchToolStripMenuItem.Text = "Google Search";
+            this.googleSearchToolStripMenuItem.Click += new System.EventHandler(this.googleSearchToolStripMenuItem_Click);
+            // 
+            // privacyPolicy
+            // 
+            this.privacyPolicy.Name = "privacyPolicy";
+            this.privacyPolicy.Size = new System.Drawing.Size(224, 26);
+            this.privacyPolicy.Text = "Privacy Policy";
+            this.privacyPolicy.Click += new System.EventHandler(this.privacyPolicyToolStripMenuItem_Click);
+            // 
+            // infoTool
+            // 
+            this.infoTool.Name = "infoTool";
+            this.infoTool.Size = new System.Drawing.Size(224, 26);
+            this.infoTool.Text = "Info";
+            this.infoTool.Click += new System.EventHandler(this.infoTool_Click);
+            // 
+            // textArea
+            // 
+            this.textArea.Location = new System.Drawing.Point(0, 31);
+            this.textArea.Name = "textArea";
+            this.textArea.Size = new System.Drawing.Size(653, 442);
+            this.textArea.TabIndex = 1;
+            this.textArea.Text = "";
+            // 
+            // fontToolStripMenuItem
+            // 
+            this.fontToolStripMenuItem.Name = "fontToolStripMenuItem";
+            this.fontToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.fontToolStripMenuItem.Text = "Font";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(653, 469);
+            this.Controls.Add(this.textArea);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Notepad";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem file;
+        private System.Windows.Forms.ToolStripMenuItem open;
+        private System.Windows.Forms.ToolStripMenuItem save;
+        private System.Windows.Forms.ToolStripMenuItem saveAs;
+        private System.Windows.Forms.ToolStripMenuItem exit;
+        private System.Windows.Forms.ToolStripMenuItem edit;
+        private System.Windows.Forms.ToolStripMenuItem find;
+        private System.Windows.Forms.ToolStripMenuItem replaceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pasteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem format;
+        private System.Windows.Forms.ToolStripMenuItem googleSearchToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem about;
+        private System.Windows.Forms.ToolStripMenuItem privacyPolicy;
+        private System.Windows.Forms.ToolStripMenuItem infoTool;
+        private System.Windows.Forms.RichTextBox textArea;
+        private System.Windows.Forms.ToolStripMenuItem fontToolStripMenuItem;
+    }
+}
+
---
+++ Notepad/Notepad/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Notepad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            //Used to Exit The Application
+            System.Environment.Exit(0);
+        }
+
+        private void privacyPolicyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            MessageBox.Show("Notepad Apllication.(Open Source)", "Privacy Policy @ 2020");
+        }
+
+        private void infoTool_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("The Notepad Application is an open source application, which is inspired by the original Notepad @ 2020","Information");
+        }
+
+        private void findToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //retireving the data from the notepad
+            String dataText = textArea.Text;
+            
+        }
+
+        private void googleSearchToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (textArea.Text.Equals(""))
+            {
+                MessageBox.Show("Nothing To Search !","Null Value !");
+            }
+            else
+            {
+                String data = textArea.Text;
+                System.Diagnostics.Process.Start(@"C:\Program Files (x86)\Google\Chrome\Application\Chrome.exe", "http://www.google.com.au/search?q="+data);
+            }
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog.FilterIndex = 2;
+            saveFileDialog.RestoreDirectory = true;
+           
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                
+            }
+        }
+    }
+    }
+
