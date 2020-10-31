4ae9600fb1591d6a37733c6d925322354b3c36f0
Sharath Sunil <alphatrinity17@gmail.com>
2020-10-31 20:04:51 +0530

    The Find and Replace Option Yet To be Set

--- Notepad/Notepad/Form1.Designer.cs
+++ Notepad/Notepad/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -50,13 +51,14 @@
             this.googleSearchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.fontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.wordWrapToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.highlightTextToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.privacyPolicyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.colorDialog1 = new System.Windows.Forms.ColorDialog();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.highlightTextToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timeAndDateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -154,7 +156,8 @@
             this.cutToolStripMenuItem,
             this.colorToolStripMenuItem,
             this.findToolStripMenuItem,
-            this.replaceToolStripMenuItem});
+            this.replaceToolStripMenuItem,
+            this.timeAndDateToolStripMenuItem});
             this.editToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.editToolStripMenuItem.Name = "editToolStripMenuItem";
             this.editToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.A)));
@@ -166,7 +169,7 @@
             this.undoToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI Light", 9F);
             this.undoToolStripMenuItem.Name = "undoToolStripMenuItem";
             this.undoToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Z)));
-            this.undoToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.undoToolStripMenuItem.Size = new System.Drawing.Size(233, 26);
             this.undoToolStripMenuItem.Text = "Undo";
             this.undoToolStripMenuItem.Click += new System.EventHandler(this.undoToolStripMenuItem_Click);
             // 
@@ -175,7 +178,7 @@
             this.redoToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI Light", 9F);
             this.redoToolStripMenuItem.Name = "redoToolStripMenuItem";
             this.redoToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Y)));
-            this.redoToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.redoToolStripMenuItem.Size = new System.Drawing.Size(233, 26);
             this.redoToolStripMenuItem.Text = "Redo";
             this.redoToolStripMenuItem.Click += new System.EventHandler(this.redoToolStripMenuItem_Click);
             // 
@@ -185,7 +188,7 @@
             this.selectAllToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI Light", 9F);
             this.selectAllToolStripMenuItem.Name = "selectAllToolStripMenuItem";
             this.selectAllToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.A)));
-            this.selectAllToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.selectAllToolStripMenuItem.Size = new System.Drawing.Size(233, 26);
             this.selectAllToolStripMenuItem.Text = "Select All";
             this.selectAllToolStripMenuItem.Click += new System.EventHandler(this.selectAllToolStripMenuItem_Click);
             // 
@@ -195,16 +198,17 @@
             this.copyToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI Light", 9F);
             this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
             this.copyToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
-            this.copyToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(233, 26);
             this.copyToolStripMenuItem.Text = "Copy";
             this.copyToolStripMenuItem.Click += new System.EventHandler(this.copyToolStripMenuItem_Click);
             // 
             // pasteToolStripMenuItem
             // 
+            this.pasteToolStripMenuItem.Enabled = false;
             this.pasteToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI Light", 9F);
             this.pasteToolStripMenuItem.Name = "pasteToolStripMenuItem";
             this.pasteToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.V)));
-            this.pasteToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.pasteToolStripMenuItem.Size = new System.Drawing.Size(233, 26);
             this.pasteToolStripMenuItem.Text = "Paste";
             this.pasteToolStripMenuItem.Click += new System.EventHandler(this.pasteToolStripMenuItem_Click);
             // 
@@ -214,7 +218,7 @@
             this.cutToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI Light", 9F);
             this.cutToolStripMenuItem.Name = "cutToolStripMenuItem";
             this.cutToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
-            this.cutToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.cutToolStripMenuItem.Size = new System.Drawing.Size(233, 26);
             this.cutToolStripMenuItem.Text = "Cut";
             this.cutToolStripMenuItem.Click += new System.EventHandler(this.cutToolStripMenuItem_Click);
             // 
@@ -224,7 +228,7 @@
             this.colorToolStripMenuItem.Name = "colorToolStripMenuItem";
             this.colorToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
             | System.Windows.Forms.Keys.C)));
-            this.colorToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.colorToolStripMenuItem.Size = new System.Drawing.Size(233, 26);
             this.colorToolStripMenuItem.Text = "Color";
             this.colorToolStripMenuItem.Click += new System.EventHandler(this.colorToolStripMenuItem_Click);
             // 
@@ -234,7 +238,7 @@
             this.findToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI Light", 9F);
             this.findToolStripMenuItem.Name = "findToolStripMenuItem";
             this.findToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.F)));
-            this.findToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.findToolStripMenuItem.Size = new System.Drawing.Size(233, 26);
             this.findToolStripMenuItem.Text = "Find";
             this.findToolStripMenuItem.Click += new System.EventHandler(this.findToolStripMenuItem_Click);
             // 
@@ -244,8 +248,9 @@
             this.replaceToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI Light", 9F);
             this.replaceToolStripMenuItem.Name = "replaceToolStripMenuItem";
             this.replaceToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.R)));
-            this.replaceToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.replaceToolStripMenuItem.Size = new System.Drawing.Size(233, 26);
             this.replaceToolStripMenuItem.Text = "Replace";
+            this.replaceToolStripMenuItem.Click += new System.EventHandler(this.replaceToolStripMenuItem_Click);
             // 
             // formatToolStripMenuItem
             // 
@@ -265,7 +270,7 @@
             this.googleSearchToolStripMenuItem.Name = "googleSearchToolStripMenuItem";
             this.googleSearchToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Alt) 
             | System.Windows.Forms.Keys.G)));
-            this.googleSearchToolStripMenuItem.Size = new System.Drawing.Size(259, 26);
+            this.googleSearchToolStripMenuItem.Size = new System.Drawing.Size(261, 26);
             this.googleSearchToolStripMenuItem.Text = "Google Search";
             this.googleSearchToolStripMenuItem.Click += new System.EventHandler(this.googleSearchToolStripMenuItem_Click);
             // 
@@ -275,7 +280,7 @@
             this.fontToolStripMenuItem.Name = "fontToolStripMenuItem";
             this.fontToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Alt) 
             | System.Windows.Forms.Keys.F)));
-            this.fontToolStripMenuItem.Size = new System.Drawing.Size(259, 26);
+            this.fontToolStripMenuItem.Size = new System.Drawing.Size(261, 26);
             this.fontToolStripMenuItem.Text = "Font";
             this.fontToolStripMenuItem.Click += new System.EventHandler(this.fontToolStripMenuItem_Click);
             // 
@@ -284,9 +289,19 @@
             this.wordWrapToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI Light", 9F);
             this.wordWrapToolStripMenuItem.Name = "wordWrapToolStripMenuItem";
             this.wordWrapToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.W)));
-            this.wordWrapToolStripMenuItem.Size = new System.Drawing.Size(259, 26);
+            this.wordWrapToolStripMenuItem.Size = new System.Drawing.Size(261, 26);
             this.wordWrapToolStripMenuItem.Text = "Word Wrap";
             this.wordWrapToolStripMenuItem.Click += new System.EventHandler(this.wordWrapToolStripMenuItem_Click);
+            // 
+            // highlightTextToolStripMenuItem
+            // 
+            this.highlightTextToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI Light", 9F);
+            this.highlightTextToolStripMenuItem.Name = "highlightTextToolStripMenuItem";
+            this.highlightTextToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.H)));
+            this.highlightTextToolStripMenuItem.Size = new System.Drawing.Size(261, 26);
+            this.highlightTextToolStripMenuItem.Text = "Highlight Text";
+            this.highlightTextToolStripMenuItem.Click += new System.EventHandler(this.highlightTextToolStripMenuItem_Click);
             // 
             // helpToolStripMenuItem
             // 
@@ -330,15 +345,14 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
-            // highlightTextToolStripMenuItem
+            // timeAndDateToolStripMenuItem
             // 
-            this.highlightTextToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI Light", 9F);
-            this.highlightTextToolStripMenuItem.Name = "highlightTextToolStripMenuItem";
-            this.highlightTextToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
-            | System.Windows.Forms.Keys.H)));
-            this.highlightTextToolStripMenuItem.Size = new System.Drawing.Size(261, 26);
-            this.highlightTextToolStripMenuItem.Text = "Highlight Text";
-            this.highlightTextToolStripMenuItem.Click += new System.EventHandler(this.highlightTextToolStripMenuItem_Click);
+            this.timeAndDateToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI Light", 9F);
+            this.timeAndDateToolStripMenuItem.Name = "timeAndDateToolStripMenuItem";
+            this.timeAndDateToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.T)));
+            this.timeAndDateToolStripMenuItem.Size = new System.Drawing.Size(233, 26);
+            this.timeAndDateToolStripMenuItem.Text = "Time And Date";
+            this.timeAndDateToolStripMenuItem.Click += new System.EventHandler(this.timeAndDateToolStripMenuItem_Click);
             // 
             // Form1
             // 
@@ -347,6 +361,7 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Notepad";
@@ -388,6 +403,7 @@
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.ToolStripMenuItem wordWrapToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem highlightTextToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem timeAndDateToolStripMenuItem;
     }
 }
 
--- Notepad/Notepad/Form1.cs
+++ Notepad/Notepad/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Drawing.Printing;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -21,20 +22,22 @@ namespace Notepad
         public Form1()
         {
             InitializeComponent();
+            //This is used to check if the File is Saved/Modified in any way 
             saved = true;
         }
-
+        //Shows A Message Box(Privacy Policy : ))
         private void privacyPolicyToolStripMenuItem_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Notepad is an Open Source Application, made to demonstrate the String and Windows Form Features.", "Pricacy Policy");
         }
-
+        //Shows A Message Box(Info Box )
         private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Notepad 2020 is made with C#.(Visual Studio)", "About Notepad");
 
         }
-
+        /*When Click The Format Menu Item->Google Search, We are redirected to the chrome browser with google search query
+         http://www.google.com.au/search?q= richTextBox.Text*/
         private void googleSearchToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
@@ -45,45 +48,54 @@ namespace Notepad
             }
             else
             {
+                //If the search box is not empty then we can search whatever that is written in the notepad
                 String queryString = "http://www.google.com.au/search?q=";
+                //A Chrome Process is initiated !, Second parameter is the Query String 
                 System.Diagnostics.Process.Start(@"C:\Program Files (x86)\Google\Chrome\Application\Chrome.exe", queryString+richTextBox1.Text.Replace(' ','+'));
             }
         }
-
+        //Cut Action
         private void cutToolStripMenuItem_Click(object sender, EventArgs e)
         {
             richTextBox1.Cut();
         }
-
+        //Paste Action
         private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
         {
             richTextBox1.Paste();
         }
-
+        //Select All Action
         private void selectAllToolStripMenuItem_Click(object sender, EventArgs e)
         {
             richTextBox1.SelectAll();
         }
-
+        //Copy Action
         private void copyToolStripMenuItem_Click(object sender, EventArgs e)
         {
             richTextBox1.Copy();
         }
-
+        //Checks whether there is a change in the text of the notepad !
         private void richTextBox1_TextChanged(object sender, EventArgs e)
         {
             if (richTextBox1.Text.Length>0)
             {
+                /*
+                By Default Cut,copy find,replace and paste options are 
+                Dislabled, When There is a Change in the Text Box , then 
+                the items will Be Enabled !*/
                 copyToolStripMenuItem.Enabled = true;
                 findToolStripMenuItem.Enabled = true;
                 cutToolStripMenuItem.Enabled = true;
                 selectAllToolStripMenuItem.Enabled = true;
                 findToolStripMenuItem.Enabled = true;
                 redoToolStripMenuItem.Enabled = true;
+                pasteToolStripMenuItem.Enabled = true;
+                replaceToolStripMenuItem.Enabled = true;
+                //When there is an event, it should be saved !
                 saved = false;
             }
         }
-
+        //Color Dialog will be opened and the Selected Color Will Be Applied to the Text in The Text Field !
         private void colorToolStripMenuItem_Click(object sender, EventArgs e)
         {
             colorDialog1.FullOpen = true;
@@ -93,10 +105,12 @@ namespace Notepad
                 
             }
         }
-
+        //Opens File Using The OpenFile Dialog Box !
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
             //Opening the Dialog Box with Filter(s) of Txt Files
+            //Filter Applied to Only Show The .txt Files !
+            //Cannot Select Multiple Files !
             using (OpenFileDialog openFileDialog=new OpenFileDialog() {Filter="Text Document|*.txt",ValidateNames=true,Multiselect=false })
             {
                 //If Ok Button is pressed then, the selected file will be Opened!
@@ -113,12 +127,12 @@ namespace Notepad
                 }
             }
         }
-
+        //Calls The Save Funcion 
         private void saveToolStripMenuItem_Click(object sender, EventArgs e)
         {
             save();
         }
-
+        //Saves the File in the background if it is saved earlier, and saves in a dialog box if it is saving for the fist time !
         private void save()
         {
             //If the File is already saved then not need to save it again and it will be saved in the background !
@@ -132,7 +146,9 @@ namespace Notepad
                 }
             }
             else
+
             {
+                //if the file is saved for the first time thenwe have to save it via save Dialog Box !
                 using (SaveFileDialog saveFileDialog = new SaveFileDialog() { Filter = "Text Documents|*.txt", ValidateNames = true })
                 {
 
@@ -157,7 +173,7 @@ namespace Notepad
         {
             saveAs();
         }
-
+        //The Save As Dialog Will Save File By Opening The Dialog Box no matter the file is saved earlier or not !
         private void saveAs()
         {
             using (SaveFileDialog saveFileDialog = new SaveFileDialog() { Filter = "Text Documents|*.txt", ValidateNames = true })
@@ -277,7 +293,18 @@ namespace Notepad
             richTextBox1.SelectionBackColor = Color.Yellow;
         }
 
+       
+
+        private void timeAndDateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            String DateTimeInfo = $"Current Date :  {DateTime.Now.DayOfWeek.ToString()},  {DateTime.Now.Day.ToString()}/{DateTime.Now.Month}/{DateTime.Now.Year.ToString()}" +
+                $"\nCurrent Time : {DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}";
+            richTextBox1.Text = DateTimeInfo;
+        }
         private void findToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+        private void replaceToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
         }
