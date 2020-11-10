cda56c8491163b8e0fac0f2ba24abf44d6ae4814
Sharath Sunil <alphatrinity17@gmail.com>
2020-11-10 13:40:01 +0530

    Question 7 of the Lab Sheet Windows Form with Increment by One Done

---
+++ Windows_Form_10/Increment/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Increment
+{
+    public partial class Form1 : Form
+    {
+        int num;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_MouseEnter(object sender, EventArgs e)
+        {
+
+            label1.Text = Convert.ToString(int.Parse(label1.Text)+1);
+        }
+    }
+}
