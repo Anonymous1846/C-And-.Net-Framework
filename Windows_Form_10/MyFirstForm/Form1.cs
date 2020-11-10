79dad5001ffb6e2c860008cf03281fac1ffe9e54
Sharath Sunil <alphatrinity17@gmail.com>
2020-11-10 14:11:54 +0530

    A very simple application where the form starts from the centre and backgroud is red and the maximize buttons are gone questin 1

---
+++ Windows_Form_10/MyFirstForm/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFirstForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //The Staring Position of the Windows form can be changed at the 
+            //StartPosition of the Properties of the Form
+            //ControlBox can be used to remove the maximize and minimize properties
+
+        }
+    }
+}
