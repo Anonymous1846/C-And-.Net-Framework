4e6b8fc91136c62fdc2e1e4387d023daa239ed70
Sharath Sunil <alphatrinity17@gmail.com>
2020-11-10 15:02:59 +0530

    AddressInfo Question Number 4 done With No Error

---
+++ Windows_Form_10/AddressInfo/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddressInfo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String addressInfoString = "Name:\t" + name.Text + "\n" +
+                "Gender:\t"+gender.Text+"\nHouse\nName:\t"+house.Text+"\n" +
+                "Locality:\t"+locality.Text+"\nPin:\t"+pin.Text+"\n" +
+                "Mobile:\t"+mobile.Text;
+            addressInfo.Text = addressInfoString;
+        }
+    }
+}
---
+++ Windows_Form_10/ConsoleWindows/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Windows.Forms;
+namespace ConsoleWindows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
